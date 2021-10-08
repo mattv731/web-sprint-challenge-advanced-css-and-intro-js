96f5170c949b9024724b67b707e40ed6176f3a1c
abc <abc@example.com>
2021-10-08 09:33:56 -0600

    I finished the sprintgit add .git add .!

--- index.js
+++ index.js
@@ -292,8 +292,15 @@ Use addArtist to do the following:
 
 Example: addArtist(artists) should return the artists array with the above object added to the end of the array. */
 
-function addArtist(/*Your Code Here*/) {
-  /*Your Code Here*/
+function addArtist(array) {
+  array.push({ 
+    id: 20,
+    name: "Matthew Von Gunten", 
+    years: 1999 - 2021,
+    genre: "Web Design", 
+    nationality: "American",
+    bio: "I like working on cars and making websites!",})
+  return array
 }
 
 
@@ -305,8 +312,14 @@ Use lotsOfArt to do the following:
 
 For example lotsOfArt(artists); will return ["Amedeo Modigliani", "Rene Magritte", ... "Albrecht Dürer"]*/
 
-function lotsOfArt(/*Your Code Here*/) {
-  /*Your Code Here*/
+function lotsOfArt(array) {
+  const over100 = [];
+  for (let i = 0; i < array.length; i++){
+    if (array[i].paintings >= 100){
+      over100.push(array[i].name)
+    }
+  }
+  return over100;
 }
 
 
